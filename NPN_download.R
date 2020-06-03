ee7d0ee206e2837aceb2bcf043dd9eadb71a0392
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2020-06-03 11:18:08 -0500

    Created NPN download

---
+++ NPN_download.R
@@ -0,0 +1,18 @@
+#This MUST BE DOWNLOADED FROM GITHUB
+#The CRAN version does not have the full capabilities needed for our project.
+library(rnpn)
+
+#setting file path
+path.doc <- "C:/Users/lucie/Documents/NPN_data/"
+
+#Retrieving npn data
+#rnpn packages has tools to show the corresponding id's for these queries. Request source is your name/affiliation
+#Phenophase 498 is colored leaves
+#network 720 is the morton arboretum
+dat.npn <- npn_download_status_data(request_source='Morton Arboretum', years=c(2017:2019), 
+                                    network_ids = c(720), phenophase_ids =c(498))
+
+write.csv(dat.npn, file.path(path.doc, file = "Arb_NPN_data_raw"), row.names=FALSE)
+
+
+
